6b062351c15e314dcb00b02cbb7e1a07b8a888b9
13mint <13mint07pineapple@gmail.com>
2026-02-23 22:01:53 +0900

    RoleRepo create

--- src/main/java/web/security/UserDetailService.java
+++ src/main/java/web/security/UserDetailService.java
@@ -1,4 +1,5 @@
 package web.security;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
