47af46eb627feffff8dd5fb7ee430adfaf83cf98
13mint <13mint07pineapple@gmail.com>
2026-02-23 23:32:41 +0900

    replace userservice to userrepo

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,18 +1,34 @@
 package web.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import web.model.AppUser;
+import web.repository.UserRepository;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @GetMapping
-    public String userPage(Model model,
-                           @AuthenticationPrincipal UserDetails userDetails){
-        model.addAttribute("user",userDetails);
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void UserPageController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user")
+    public String userPage(Model model, Principal principal) {
+
+        Optional<AppUser> user = userRepository.findByUsername(principal.getName());
+
+        model.addAttribute("user", user);
+
         return "user";
     }
 }
