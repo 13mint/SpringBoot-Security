fa386fd118c23e30f027a2b351430db39c6a9a93
13mint <13mint07pineapple@gmail.com>
2026-02-27 19:36:57 +0900

    replace config to web

--- src/main/java/web/config/MvcConfig.java
+++ src/main/java/web/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package config;
+package web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
--- src/main/java/web/config/SuccessUserHandler.java
+++ src/main/java/web/config/SuccessUserHandler.java
@@ -1,4 +1,4 @@
-package config;
+package web.config;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
