7ccbd8c4269b65ce03f931b0e6b3f5c9d056f834
13mint <13mint07pineapple@gmail.com>
2026-03-05 12:43:20 +0900

    implements method into Role

--- src/main/java/web/model/Role.java
+++ src/main/java/web/model/Role.java
@@ -1,12 +1,13 @@
 package web.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,4 +46,8 @@ public class Role {
     }
 
 
+    @Override
+    public String getAuthority() {
+        return "";
+    }
 }
